17e3a4d6f025b65793a0429ef32bc25d18cbaf48
Robert <mail@rbeekman.nl>
2025-12-25 08:22:24 +0100

    Update for latest Artemis and HomeWizard JSON format
    Includes additional P1 meter data.
    Updates dependencies to the latest versions.
    Enforces minimum Artemis version.

---
+++ src/Responses/ExternalMeter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Artemis.Plugins.Modules.HomeWizard.Responses;
+
+public class ExternalMeter
+{
+    [JsonPropertyName("unique_id")]
+    public string UniqueId { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("timestamp")]
+    public long Timestamp { get; set; }
+
+    [JsonPropertyName("value")]
+    public double Value { get; set; }
+
+    [JsonPropertyName("unit")]
+    public string Unit { get; set; }
+}
